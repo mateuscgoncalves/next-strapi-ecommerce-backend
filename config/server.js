afe7e4fcdc0394770459e82eb42b504e48569ff3
Mateus Gonçalves <mateus_c_g@hotmail.com>
2020-11-27 11:50:27 +0100

    created strapi in version 3.3.2 and product collection type

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'abdb4ae4952e846a81c3944063e106c9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bbc7f6c3-dd9e-4d3c-8cdb-02e9a32594b8'
+};
